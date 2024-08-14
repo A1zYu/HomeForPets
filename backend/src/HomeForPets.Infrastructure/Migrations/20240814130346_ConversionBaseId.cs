e9ebab8394f5b8167f87869849e385dfcf5ea129
A1zYu <2yusupchik1@mail.ru>
2024-08-14 16:07:05 +0300

    migrations

---
+++ backend/src/HomeForPets.Infrastructure/Migrations/20240814130346_ConversionBaseId.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HomeForPets.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConversionBaseId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_payment_details_pet_pet_id",
+                table: "payment_details");
+
+            migrationBuilder.DropForeignKey(
+                name: "fk_pet_photo_pet_pet_id",
+                table: "pet_photo");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_payment_details_pets_pet_id",
+                table: "payment_details",
+                column: "pet_id",
+                principalTable: "pets",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_pet_photo_pets_pet_id",
+                table: "pet_photo",
+                column: "pet_id",
+                principalTable: "pets",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_payment_details_pets_pet_id",
+                table: "payment_details");
+
+            migrationBuilder.DropForeignKey(
+                name: "fk_pet_photo_pets_pet_id",
+                table: "pet_photo");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_payment_details_pet_pet_id",
+                table: "payment_details",
+                column: "pet_id",
+                principalTable: "pets",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_pet_photo_pet_pet_id",
+                table: "pet_photo",
+                column: "pet_id",
+                principalTable: "pets",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
