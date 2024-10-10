8911d9075f6f3cf1782f827e28e62ad6633f9bdf
A1zYu <2yusupchik1@mail.ru>
2024-10-10 21:13:32 +0300

    fix

--- backend/src/HomeForPets.Api/Program.cs
+++ backend/src/HomeForPets.Api/Program.cs
@@ -23,35 +23,8 @@ Log.Logger = new LoggerConfiguration()
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title = "My API",
-        Version = "v1"
-    });
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        In = ParameterLocation.Header,
-        Description = "Please insert JWT with Bearer into field",
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey
-    });
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            new string[] { }
-        }
-    });
-});
+
+builder.Services.AddCustomSwagger();
 
 builder.Services.AddSerilog();
 
